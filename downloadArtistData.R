b475b160d327ad628a10772bc32fca1de67a9bb5
Johan Wållgren <johan.g.wallgren@gmail.com>
2018-04-22 14:28:35 +0200

    Update downloadArtistData.R

--- downloadArtistData.R
+++ downloadArtistData.R
@@ -7,8 +7,8 @@ artistList <- c('Avicii','Drake','The Weeknd','Ed Sheeran','Marshmello','Kendric
                 'The Chainsmokers','Calvin Harris','Eminem','Shawn Mendes','Daddy Yankee',
                 'Bruno Mars','Chris Brown','Maroon 5')
 
-Sys.setenv(SPOTIFY_CLIENT_ID = '21fae2a08aad434d9da510b25f09de90')
-Sys.setenv(SPOTIFY_CLIENT_SECRET = '8d3588f452d445329ab8d1c5d8e5cb7a')
+Sys.setenv(SPOTIFY_CLIENT_ID = '21fae2a08aad434d9da510b25f09de90') # Not my real client id
+Sys.setenv(SPOTIFY_CLIENT_SECRET = '8d3588f452d445329ab8d1c5d8e5cb7a') # Not my real client secret
 
 access_token <- get_spotify_access_token()
 
@@ -16,6 +16,7 @@ artistData <- as_tibble()
 
 for (i in 1:length(artistList)){
   
+  # Sleeps to avoid crash of download
   if(i %% 3 == 0){
     print('--Resting for 5 seconds--')
     Sys.sleep(5)
@@ -29,8 +30,5 @@ for (i in 1:length(artistList)){
   rm(temp)
 }
 
-
-
-
-
+save(artistData, file = 'artistData.RData')
 
