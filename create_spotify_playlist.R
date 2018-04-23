234fee1fc9f66dac8bd6bcae49104edc7b171a43
Johan Wållgren <johan.g.wallgren@gmail.com>
2018-04-23 16:30:04 +0200

    Update create_spotify_playlist.R

--- create_spotify_playlist.R
+++ create_spotify_playlist.R
@@ -2,38 +2,78 @@ library(spotifyr)
 library(tidyverse)
 library(httr)
 
+# These expire, needs to be updated
+access_tokenCreate <- 'BQC9lzahE-1bqjPj9Z1ITKDjXaUe5JX-6KWN1aMR-LwTZ1YcUCXuUpoth9H5zyM36bchHM4jM6p6AckteM_XuQVIe9lQu5jrAF-463LsxuEMSOr4XhgdOWJcD5PfEq654M3X_YXAe335MrYYc5H5Y-6YYyrG9i58q2QHQlUbV-j6Az6u8sXXFp7kOtN4KgOEIygG5zoxHEmEGS0wtIClzA9Z2br73to5sGWep9E'
+access_tokenAdd <- 'BQDTAk0y4regGeiqTMXXjAekjN2FBpsaepFBcO_csuVWYoyLvXoSYwK-tb3XfQG-IIIhSbIVB-J-5emLUYKitwmAm8JHJhzrzNPwXTPfnaeIzHYDkd4cHbLeAaihMOLdyGYcOuzm4eV3y2WrTOsopUqIOdpiGDsSMPaoITQo2YDVuKuRo2K3wfuCpNSfVTtmOzv9Kz02Tp8Y5SsppOBxTTzOOas7kdWqPPQNz_Y'
 
-# access_token = get_spotify_access_token()
-access_token <- ''
-
+# User information
+user <- 'dn1mre3xxwdhwg9m9oxoes8tu'
 Sys.setenv(SPOTIFY_CLIENT_ID = '21fae2a08aad434d9da510b25f09de90')
 Sys.setenv(SPOTIFY_CLIENT_SECRET = '8d3588f452d445329ab8d1c5d8e5cb7a')
-user <- 'dn1mre3xxwdhwg9m9oxoes8tu'
 
+# Name of playlist to be created
+playlistName <- 'MAWS01'
 
+load('./artistData.RData')
+ad <- artistData
+
+#######################################################
+# Create playlist
 url <- paste0("https://api.spotify.com/v1/users/", user, "/playlists")
 
-res <- POST(url,
-            accept_json(),
-            body = list(name = "New Playlist12",
-                             public = "true",
-                             access_token = access_token),
-            encode = 'form',
-            httr::config(http_version = 2),
-            verbose()) %>% 
+bodyText <- paste0('{"name": "', playlistName,'","public": true}')
+
+res <- POST(
+  url,
+  accept_json(),
+  query = list(access_token = access_tokenCreate),
+  body = bodyText,
+            content_type_json(),
+  encode = 'form',
+  httr::config(http_version = 2),
+  verbose()
+) %>%
   content
 
+#######################################################
+# Get playlist data
+# File copied from Rcharlie, https://github.com/charlie86/ since not in library yet
+userPlaylists<- get_user_playlists(user)
 
-# post <- POST('https://accounts.spotify.com/api/token',
-#              accept_json(), authenticate(client_id, client_secret),
-#              body = list(grant_type = 'client_credentials'),
-#              encode = 'form', httr::config(http_version = 2)) %>% content
+# Selecting all playlists with name == playlistName
+playlistURIs <- filter(userPlaylists, playlist_name == playlistName) %>% 
+  select(playlist_uri)
 
+# The top playlistURI will be used
+playlistURI <- playlistURIs$playlist_uri[1]
 
-# vec_happy <- as.vector(happy_songs$id)
-# for(i in 1:length(vec_happy)) {
-#   URL_temp <- paste0('https://api.spotify.com/v1/users/', userID, 
-#                      '/playlists/', playlistID_happy, '/tracks?uris=spotify:track:', vec_happy[i])
-#   POST(url = URL_temp, add_headers(Authorization = hv, scope = 'playlist-modify-public'
-#   ))
-# }
+#######################################################
+# Add track to playlist
+# Adds 50 songs to playlist
+trackURIs <- c(ad$track_uri[1:50])
+textBeforeTracks <- 'spotify%3Atrack%3A'
+
+trackURIsText <- ''
+for(i in 1:length(trackURIs)){
+  
+  trackURIsText <- paste0(trackURIsText, textBeforeTracks, trackURIs[i])
+  
+  if(i != length(trackURIs)){
+    trackURIsText <- paste0(trackURIsText,',')
+  }
+}
+
+url <- paste0("https://api.spotify.com/v1/users/", user, "/playlists/",playlistURI,
+              '/tracks?uris=', trackURIsText)
+
+res <- POST(
+  url,
+  accept_json(),
+  query = list(access_token = access_tokenAdd),
+  content_type_json(),
+  encode = 'form',
+  httr::config(http_version = 2),
+  verbose()
+) %>%
+  content
+
