8ff406c06275d5de7923a19e29f3dca7ed565a77
Johan Wållgren <johan.g.wallgren@gmail.com>
2018-04-24 21:52:49 +0200

    Add files via upload

---
+++ App/server.R
@@ -0,0 +1,193 @@
+# server
+
+server <- function(input, output) {
+  ################################################
+  # Reactive function returning table to plot
+  filterAd <- reactive({
+    
+    ad <- createAd()
+    
+    ad <<- filter(
+      ad,
+      energy >= input$energy[1] &
+        energy <= input$energy[2] &
+        loudness >= input$loudness[1] &
+        loudness <= input$loudness[2] &
+        speechiness >= input$speechiness[1] &
+        speechiness <= input$speechiness[2] &
+        acousticness >= input$acousticness[1] &
+        acousticness <= input$acousticness[2] &
+        instrumentalness >= input$instrumentalness[1] &
+        instrumentalness <= input$instrumentalness[2] &
+        liveness >= input$liveness[1] &
+        liveness <= input$liveness[2] &
+        valence >= input$valence[1] &
+        valence <= input$valence[2] &
+        tempo >= input$tempo[1] &
+        tempo <= input$tempo[2] &
+        danceability >= input$danceability[1] &
+        danceability <= input$danceability[2]
+    )
+  })
+  ################################################
+  createAd <- eventReactive(input$importRelatedArtist, {
+    
+    print('Retreaving related artists!')
+    
+    relatedArtists <- get_related_artists(input$startArtist)
+    allArists <- c(input$startArtist, relatedArtists$artist_name)
+    
+    access_token <- get_spotify_access_token()
+    
+    artistData <- as_tibble()
+    
+    for (i in 1:10) {
+      Sys.sleep(2)
+      print(paste(i, allArists[i]))
+      
+      # Does not work if Spotify does not have any tracks with related artist
+      temp <- as_tibble(get_artist_audio_features_mod(allArists[i]))
+      
+      if(is.na(temp) == 0){
+        
+        temp <- mutate(temp, 'artist' = allArists[i])
+        
+        artistData <- bind_rows(artistData, temp)
+        rm(temp)
+        
+      }
+
+    }
+    # energy
+    # loudness
+    # speechiness
+    # acousticness
+    # instrumentalness
+    # liveness
+    # valence
+    # tempo
+    # danceability
+    # track_popularity
+    
+    print('Done retreaving related artists!')
+    
+    artistData %>%
+      mutate(energy = normalizeData(energy)) %>%
+      mutate(loudness = normalizeData(loudness)) %>%
+      mutate(speechiness = normalizeData(speechiness)) %>%
+      mutate(acousticness = normalizeData(acousticness)) %>%
+      mutate(instrumentalness = normalizeData(instrumentalness)) %>%
+      mutate(liveness = normalizeData(liveness)) %>%
+      mutate(valence = normalizeData(valence)) %>%
+      mutate(tempo = normalizeData(tempo)) %>%
+      mutate(danceability = normalizeData(danceability)) %>%
+      mutate(track_popularity = normalizeData(track_popularity))
+    
+  })
+  
+  ################################################
+  output$tableOfSongs <- renderTable({
+    outputTable <- filterAd()
+    
+    outputTable <-
+      select(outputTable,
+             artist,
+             track_name,
+             album_name,
+             track_popularity) %>%
+      arrange(desc(track_popularity)) %>%
+      select(-track_popularity) %>%
+      rename('Artist' = artist,
+             'Track' = track_name,
+             'Album' = album_name)
+    
+    head(outputTable, 50)
+    
+  })
+  
+  ################################################
+  ################################################
+  output$doneText <- renderText({
+    doneText <- doneText()
+    doneText <- 'Done!'
+    doneText
+  })
+  
+  ################################################
+  doneText <- eventReactive(input$createPlaylist, {
+    # Code to create playlist goes here
+    
+    print('Create Playlist!')
+    
+    # Name of playlist to be created
+    playlistName <- paste('MAWS', input$startArtist)
+    
+    # Create playlist
+    url <- paste0("https://api.spotify.com/v1/users/", user, "/playlists")
+    
+    bodyText <- paste0('{"name": "', playlistName,'","public": true}')
+    
+    res <- POST(
+      url,
+      accept_json(),
+      query = list(access_token = access_tokenCreate),
+      body = bodyText,
+      content_type_json(),
+      encode = 'form',
+      httr::config(http_version = 2),
+      verbose()
+    ) %>%
+      content
+    
+    # Get playlist data
+    # File copied from Rcharlie, https://github.com/charlie86/ since not in library yet
+    userPlaylists<- get_user_playlists(user)
+    
+    # Selecting all playlists with name == playlistName
+    playlistURIs <- filter(userPlaylists, playlist_name == playlistName) %>% 
+      select(playlist_uri)
+    
+    # The first playlistURI will be used
+    playlistURI <- playlistURIs$playlist_uri[1]  
+    
+    # Add track to playlist
+    # Adds 50 songs to playlist
+    if(length(ad$track_uri) >= 50){
+      trackURIs <- c(ad$track_uri[1:50])
+    }else {
+      trackURIs <- c(ad$track_uri)
+    }
+
+    textBeforeTracks <- 'spotify%3Atrack%3A'
+    
+    trackURIsText <- ''
+    for(i in 1:length(trackURIs)){
+      
+      trackURIsText <- paste0(trackURIsText, textBeforeTracks, trackURIs[i])
+      
+      if(i != length(trackURIs)){
+        trackURIsText <- paste0(trackURIsText,',')
+      }
+    }
+    
+    url <- paste0("https://api.spotify.com/v1/users/", user, "/playlists/",playlistURI,
+                  '/tracks?uris=', trackURIsText)
+    
+    res <- POST(
+      url,
+      accept_json(),
+      query = list(access_token = access_tokenCreate),
+      content_type_json(),
+      encode = 'form',
+      httr::config(http_version = 2),
+      verbose()
+    ) %>%
+      content
+    
+    doneText <- 'Done'
+    doneText
+
+  })
+  
+  
+}
---
+++ App/ui.R
@@ -0,0 +1,150 @@
+# ui
+
+##########################################################################
+# Set initial state
+library(shiny)
+library(tidyverse)
+library(shinythemes)
+library(spotifyr)
+library(httr)
+
+source('./get_artist_audio_features_mod.R')
+source('./get_user_playlists.R')
+source('./get_related_artists.R')
+source('./normalizeData.R')
+
+# Update these two:
+# https://beta.developer.spotify.com/console/post-playlists/
+access_tokenCreate <<- 'BQB7pNmnWudTq68Uu5uzxHpNR5L9OKoXT2NOM-3rqdPV4TNdod_QKHhd7Ow9reFyYHV_Cws0gcQNgcnno80ymjPRE9c_QboCRqZRIThWegcGNbypzu27jVNSG1ln9Db2EyHbjVhWxJ8V2BU1NglUzMxXqFKUkR0FW5_9gS7yn2gdT0P2Me4FvkAn1G8YtPBH7Ufeg5rOLoSzCXusaN-bwNCixCFIWavSA_byXtI'
+
+# User information
+user <<- 'dn1mre3xxwdhwg9m9oxoes8tu'
+Sys.setenv(SPOTIFY_CLIENT_ID = '11fae2a08aad434d9da510b25f09de90') # Not my real client id
+Sys.setenv(SPOTIFY_CLIENT_SECRET = '7d3588f452d445329ab8d1c5d8e5cb7a') # Not my real client secret
+
+##########################################################################
+# UI
+ui <- fluidPage(theme = shinytheme("cyborg"),
+                titlePanel("MessingAroundWithSpotify"),
+                sidebarLayout(
+                  sidebarPanel(width = 3,
+                               ####################################
+                               # Sidebar panel
+                               
+                               textInput("startArtist", "Choose artist", "Avicii"),
+                               actionButton("importRelatedArtist", "Go!"),
+                               
+                               # energy
+                               sliderInput(
+                                 "energy",
+                                 label = "Select range for energy:",
+                                 min = -1,
+                                 max = 1,
+                                 value = c(-1,1),
+                                 sep = '',
+                                 step = 0.05,
+                                 width = '350px'
+                               ),
+                               # loudness
+                               sliderInput(
+                                 "loudness",
+                                 label = "Select range for loudness:",
+                                 min = -1,
+                                 max = 1,
+                                 value = c(-1,1),
+                                 sep = '',
+                                 step = 0.05,
+                                 width = '350px'
+                               ),
+                               # speechiness
+                               sliderInput(
+                                 "speechiness",
+                                 label = "Select range for speechiness:",
+                                 min = -1,
+                                 max = 1,
+                                 value = c(-1,1),
+                                 sep = '',
+                                 step = 0.05,
+                                 width = '350px'
+                               ),
+                               # acousticness
+                               sliderInput(
+                                 "acousticness",
+                                 label = "Select range for acousticness:",
+                                 min = -1,
+                                 max = 1,
+                                 value = c(-1,1),
+                                 sep = '',
+                                 step = 0.05,
+                                 width = '350px'
+                               ),
+                               # instrumentalness
+                               sliderInput(
+                                 "instrumentalness",
+                                 label = "Select range for instrumentalness:",
+                                 min = -1,
+                                 max = 1,
+                                 value = c(-1,1),
+                                 sep = '',
+                                 step = 0.05,
+                                 width = '350px'
+                               ),
+                               # liveness
+                               sliderInput(
+                                 "liveness",
+                                 label = "Select range for liveness:",
+                                 min = -1,
+                                 max = 1,
+                                 value = c(-1,1),
+                                 sep = '',
+                                 step = 0.05,
+                                 width = '350px'
+                               ),
+                               # valence
+                               sliderInput(
+                                 "valence",
+                                 label = "Select range for valence:",
+                                 min = -1,
+                                 max = 1,
+                                 value = c(-1,1),
+                                 sep = '',
+                                 step = 0.05,
+                                 width = '350px'
+                               ),
+                               # tempo
+                               sliderInput(
+                                 "tempo",
+                                 label = "Select range for tempo:",
+                                 min = -1,
+                                 max = 1,
+                                 value = c(-1,1),
+                                 sep = '',
+                                 step = 0.05,
+                                 width = '350px'
+                               ),
+                               # danceability
+                               sliderInput(
+                                 "danceability",
+                                 label = "Select range for danceability:",
+                                 min = -1,
+                                 max = 1,
+                                 value = c(-1,1),
+                                 sep = '',
+                                 step = 0.05,
+                                 width = '350px'
+                               )
+
+
+                  ),
+                  ####################################
+                  # Main panel
+                  
+                  mainPanel(
+                    actionButton("createPlaylist", "Playlist!"),
+                    textOutput(outputId = "doneText"),
+                    tableOutput(outputId = 'tableOfSongs')
+
+  
+                  ))
+                  
+                )
